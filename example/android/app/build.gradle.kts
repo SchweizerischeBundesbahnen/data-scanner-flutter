fe97685f601bc1d0d14c80881e6c752cd8eed53d
Nico Vidoni <30844036+smallTrogdor@users.noreply.github.com>
2025-06-26 10:32:21 +0200

    feat: bump minimum supported flutter sdk to 3.27.x (#39)
    * feat: bump minimum supported flutter sdk to 3.27.x
    * bump dsm to 3.x.x to resolve failing nightly built
    * chore: bump versions in CI
    * chore: reinit android example app setup gitignore
    * chore: reinit android example app setup - AndroidManifest
    * chore: reinit android example app setup - MainActivity
    * chore: reinit android example app setup - gradleProperties
    * chore: reinit android example app setup - gradleWrapper
    * chore: reinit android example app setup - settings gradle
    * chore: reinit android example app setup - styles
    * chore: reinit android example app setup - root build gradle
    * chore: reinit android example app setup - app build gradle
    * chore: reinit android example app setup - metadata
    * chore: reinit android example app setup - last changes
    * chore: reinit android example app setup - update AGP and Kotlin
    * chore: rm compileOptions
    * chore: rm afterSubprojectEvaluation

---
+++ example/android/app/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "ch.sbb.datascanner.example"
+    compileSdk = flutter.compileSdkVersion
+    ndkVersion = "27.0.12077973"
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "ch.sbb.datascanner.example"
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    androidResources {
+        noCompress += "tflite"
+    }
+
+}
+
+flutter {
+    source = "../.."
+}
